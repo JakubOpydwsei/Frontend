fd2054f1451239c2d1487ee2c815512ce691bf82
JakubOpyd <jakub.opyd@microsoft.wsei.edu.pl>
2024-10-25 23:26:34 +0200

    Lab3

--- src/components/FlexContainer.jsx
+++ src/components/FlexContainer.jsx
@@ -1,29 +1,59 @@
 import PersonProfile from "../components/PersonProfile.jsx";
-import { useState } from "react";
+// import { useState } from "react";
+import { useReducer } from "react";
+import AppReducer from "../data/AppReducer";
 
-
-
-function FlexContainer(element) {
-    const[personRate, changeRate] = useState(0);
-    const[personEdit, editPerson] = useState(0);
-    const[personDelete, deletePerson] = useState(0);
-    //console.log(element.element)
+function FlexContainer({element}) {
+//   const [personRate, changeRate] = useState(0);
+//   const [personEdit, editPerson] = useState(0);
+//   const [personDelete, deletePerson] = useState(0);
+  //console.log(element.element)
+  const [items, dispatch] = useReducer(AppReducer, element);
   return (
     <>
       <div className="app">
         <h1>People List:</h1>
         <div className="d-flex flex-col mb-3 flex-wrap align-content-center">
-          
-          {element.element.map((person) => (
+          {items.map((person) => (
             <div key={person.id} className="p-2 person-list ">
-            <PersonProfile key={person.id} person={person} />
-            <button onClick={() => editPerson(personEdit)}>Edit</button>
-            <button onClick={() => deletePerson(personDelete)}>Delete</button>
-            <button onClick={() => changeRate(person.rating = (person.rating + 1)%11)}>Rate</button>
-            <p>{personRate}</p> 
+              <PersonProfile key={person.id} person={person} />
+              <button
+                onClick={() => {
+                  dispatch({
+                    type: "edit",
+                    id: person.id,
+                  });
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  dispatch({
+                    type: "delete",
+                    id: person.id,
+                  });
+                }}
+              >
+                Delete
+              </button>
+              <button
+              onClick={() => {
+                dispatch({
+                        type: "rate",
+                        id: person.id,
+                        rate: person.rating
+                            });
+                        }
+                 }
+                // onClick={() =>
+                //   changeRate((person.rating = (person.rating + 1) % 11))
+                // }
+              >
+                Rate
+              </button>
             </div>
           ))}
-          
         </div>
       </div>
     </>
--- src/data/AppReducer.js
+++ src/data/AppReducer.js
@@ -1,11 +1,19 @@
+import { data } from "../lab01/module-data.js";
+
 export default function AppReducer(state, action){
     switch(action.type){
         case "edit":
             return state;
         case "rate":{
-            return state;
+            return state.map(person =>{
+                if (person.id == action.id){
+                    return { ...person, rating: (action.rate + 1 ) %11 };
+                }
+                return person;
+            });
         }
         case "delete":
-            return state;       
+            // console.log(data.filter(person => person.id != action.id))
+            return state.filter(person => person.id != action.id); 
     }
 }
